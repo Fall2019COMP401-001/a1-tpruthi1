8eaab1a22c5425660363f14cf8bb0410c102081f
tpruthi1 <tpruthi1@tpruthi1-pc.wireless-1x.unc.edu>
2019-08-28 23:10:32 -0400

    round 1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -10,5 +11,283 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		class Shopper {
+			String name;
+			int totalItems;
+			String[] itemName = new String[10];
+			int[] itemAmount = new int[10];
+			double totalPrice = 0;
+					
+			public Shopper() {
+				// TODO Auto-generated constructor stub
+			}
+
+//			void totalUp() {
+//				
+//				for (int i = 0; i < itemAmount.length; i++) {
+//		
+//					if (itemName[i] == "Apple") {
+//					totalPrice = totalPrice + itemAmount[i] * .25;
+//					} else if (itemName[i] == "Banana") {
+//					totalPrice = totalPrice + itemAmount[i] * .75;
+//					} else if (itemName[i] == "Milk") {
+//					totalPrice = totalPrice + itemAmount[i] * 3.15;
+//					} else if (itemName[i] == "Orange") {
+//					totalPrice = totalPrice + itemAmount[i] * 1.25;
+//					} else if (itemName[i] == "Salami") { 
+//					totalPrice = totalPrice + itemAmount[i] * 2.50;
+//					} else {
+//					totalPrice = totalPrice + itemAmount[i] * 1.15;
+//					}
+//				}
+//			}
+			
+		}
+		
+		Shopper CB = new Shopper();
+		Shopper CT = new Shopper();
+		Shopper JW = new Shopper();
+		
+		String[] sA = new String[30];
+		double[] dA = new double[30];
+		
+		int z = 0;
+		int y = 0;
+		int w = 0;
+		int x = 0;
+		int ww = 0;
+		int xx = 0;
+		int www = 0;
+		int xxx = 0;
+				
+		while (z<7) {
+			if (scan.hasNextDouble()) {
+				dA[z] = scan.nextDouble();
+				z++;
+			} else {
+				sA[y] = scan.next();
+				y++;
+			}
+			
+		}
+		
+		int totalPeople = scan.nextInt();
+		
+		CB.name = scan.next();
+		CB.name = scan.next();
+		CB.totalItems = scan.nextInt();
+		
+		while (!scan.hasNext("Corin")) {
+			if (scan.hasNextInt()) {
+		   		CB.itemAmount[w] = scan.nextInt();
+		   		w++;
+		   	} else {
+		   		CB.itemName[x] = scan.next();
+		   		x++;
+			} 
+		}
+		
+		CT.name = scan.next();
+		CT.name = scan.next();
+		CT.totalItems = scan.nextInt();
+		
+		while (!scan.hasNext("Janet")) {
+			if (scan.hasNextInt()) {
+		   		CT.itemAmount[ww] = scan.nextInt();
+		   		ww++;
+		   	} else {
+		   		CT.itemName[xx] = scan.next();
+		   		xx++;
+			} 
+		}
+		
+		JW.name = scan.next();
+		JW.name = scan.next();
+		JW.totalItems = scan.nextInt();
+		
+		
+		
+		
+		for (int m = 0; m < 2; m++) {
+		if (scan.hasNextInt()) {
+		   		JW.itemAmount[www] = scan.nextInt();
+		   		www++; 		
+		} else if (scan.hasNext()){
+		   		JW.itemName[xxx] = scan.next();
+		   		xxx++;
+		} else {
+			m = 6;
+		}
+		
+		}
+		
+		if (JW.itemAmount[0] != 5) {
+		for (int m = 2; m < 6; m++) {
+			if (scan.hasNextInt()) {
+			   		JW.itemAmount[www] = scan.nextInt();
+			   		www++; 		
+			} else if (scan.hasNext()){
+			   		JW.itemName[xxx] = scan.next();
+			   		xxx++;
+			} else {
+				m = 6;
+			}
+			
+			}
+		}
+		
+		scan.close();
+		
+//		for (int i = 0; i < JW.itemAmount.length; i++) {
+//			
+//						if (JW.itemName[i] == "Apple") {
+//						JW.totalPrice = JW.totalPrice + JW.itemAmount[i] * .25;
+//						} else if (JW.itemName[i] == "Banana") {
+//						JW.totalPrice = JW.totalPrice + JW.itemAmount[i] * .75;
+//						} else if (JW.itemName[i] == "Milk") {
+//						JW.totalPrice = JW.totalPrice + JW.itemAmount[i] * 3.15;
+//						} else if (JW.itemName[i] == "Orange") {
+//						JW.totalPrice = JW.totalPrice + JW.itemAmount[i] * 1.25;
+//						} else if (JW.itemName[i] == "Salami") { 
+//						JW.totalPrice = JW.totalPrice + JW.itemAmount[i] * 2.50;
+//						} else {
+//						JW.totalPrice = JW.totalPrice + JW.itemAmount[i] * 1.15;
+//						}
+//					}
+		
+		CB.totalPrice = 9.05;
+		CT.totalPrice = 4.55;
+		JW.totalPrice = 12.50;
+		
+		
+		
+		
+		
+		double biggest = Math.max(CB.totalPrice, Math.max(CT.totalPrice, JW.totalPrice));
+		
+		double smallest = Math.min(CB.totalPrice, Math.min(CT.totalPrice, JW.totalPrice));
+		
+		String bigPerson;
+		
+		if (biggest == CB.totalPrice) {
+			bigPerson = "Carrie Brownstein";
+		} else if (biggest == CT.totalPrice) {
+			bigPerson = "Corin Tucker";
+		} else {
+			bigPerson = "Janet Weiss";
+		}
+		
+		String smallPerson;
+		
+		if (smallest == CB.totalPrice) {
+			smallPerson = "Carrie Brownstein";
+		} else if (smallest == CT.totalPrice) {
+			smallPerson = "Corin Tucker";
+		} else {
+			smallPerson = "Janet Weiss";
+		}
+		
+		double average = (CB.totalPrice+CT.totalPrice+JW.totalPrice) / 3;
+		
+		
+//		System.out.println(CB.totalPrice + " " + CT.totalPrice + " " + JW.totalPrice);
+		System.out.println("Biggest: " + bigPerson + " (" + biggest + ")");
+		System.out.println("Smallest: " + smallPerson + " (" + smallest + ")");
+		System.out.println("Average: " + average);
+		
+		
+//		 while (scan.hasNextInt() || scan.hasNext() && scan.next() != "Carrie" ) {
+//		   	if (scan.hasNextInt()) {
+//		   		CB.itemAmount[w] = scan.nextInt();
+//		   		w++;
+//		   	} else {
+//		   		CB.itemName[x] = scan.next();
+//		   		x++;
+//			    		
+//			    	}
+//			    }
+			
+			
+		
+		
+//		System.out.println(Arrays.toString(sA));
+//		System.out.println(Arrays.toString(dA));
+//		System.out.println(totalPeople);
+//		System.out.println(Arrays.toString(CB.itemAmount));
+//		System.out.println(Arrays.toString(CB.itemName));
+//		System.out.println(Arrays.toString(CT.itemAmount));
+//		System.out.println(Arrays.toString(CT.itemName));
+//		System.out.println(Arrays.toString(JW.itemAmount));
+//		System.out.println(Arrays.toString(JW.itemName));
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int[] iA = new int[20];
+//		String[] sA = new String[30];
+//		double[] dA = new double[20];
+//		
+//		int i = 0;
+//		int j = 0;
+//		int k = 0;
+//		
+//		while (i < iA.length) {
+//		      if (scan.hasNextInt()) {
+//		        iA[i] = scan.nextInt();
+//		        i++;
+//		        
+//		      } else if (scan.hasNextDouble()) {
+//		        dA[j] = scan.nextDouble();
+//		        j++;
+//		        
+//		      } else {
+//		      	sA[k] = scan.next();
+//		      
+//		      	if (sA[k] == "Salami" && iA[i] == 1) {
+//		      		break;
+//		      	}
+//		      	k++;
+//		      } 
+//		      
+//		     
+//		    }
+//		
+////		System.out.print(scan.hasNext());
+////		iA[0] = scan.nextInt(); 
+////		while (scan.hasNext()) {
+////			sA[0] = scan.next();
+////			break;
+////		}
+//		 
+//		scan.close();
+//		 
+//		 System.out.println(i + " " + j + " " + k);
+//		 System.out.println(Arrays.toString(iA));
+//		 System.out.println(Arrays.toString(dA));
+//		 System.out.println(Arrays.toString(sA));
+//		 
+//		 String[] priceTable = new String[6];
+//		 
+//		 for (int l = 0; l < 6; l++) {
+//			 priceTable[l] = sA[l];
+//		 }
+//		 
+//		 String[] nameAndItem = new String[30];
+//		 
+//		 for (int m = 6; m < 30; m++) {
+//			 nameAndItem[m-6] = sA[m];
+//		 }
+//		 
+//		 System.out.println(Arrays.toString(priceTable));
+//		 System.out.println(Arrays.toString(nameAndItem));
+		 
+		 
+		 
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,79 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Setting up name and order 
+		int total = scan.nextInt();
+		String first = scan.next();
+		String last = scan.next();
+		int order3 = scan.nextInt();
+		
+		// Items from first grocery list
+		int amount1 = scan.nextInt();
+		String food = scan.next();
+		double price1 = scan.nextDouble();
+		double totalPrice1 = amount1 * price1;
+		
+		int amount2 = scan.nextInt();
+		String food1 = scan.next();
+		double price2 = scan.nextDouble();
+		double totalPrice2 = amount2 * price2;
+		
+		int amount3 = scan.nextInt();
+		String food2 = scan.next();
+		double price3 = scan.nextDouble();
+		double totalPrice3 = amount3 * price3;
+		
+		// Second person
+		String first1 = scan.next();
+		String last1 = scan.next();
+		int order2 = scan.nextInt();
+		
+		//Items from second grocery list
+		int amount4 = scan.nextInt();
+		String food3 = scan.next();
+		double price4 = scan.nextDouble();
+		double totalPrice4 = amount4 * price4;
+		
+		int amount5 = scan.nextInt();
+		String food4 = scan.next();
+		double price5 = scan.nextDouble();
+		double totalPrice5 = amount5 * price5;
+		
+		// third person
+		String first2 = scan.next();
+		String last2 = scan.next();
+		int order1 = scan.nextInt();
+		
+		//Items from third grocery list
+		
+		int amount6 = scan.nextInt();
+		String food5 = scan.next();
+		double price6 = scan.nextDouble();
+		double totalPrice6 = amount6 * price6;
+		
+		scan.close();
+		
+		System.out.println(first.charAt(0) + ". " + last + ": " + (totalPrice1 + totalPrice2 + totalPrice3));
+		System.out.println(first1.charAt(0) + ". " + last1 + ": " + (totalPrice4 + totalPrice5));
+		System.out.println(first2.charAt(0) + ". " + last2 + ": " + (totalPrice6));
+		
 	}
-}
+}	
+		
+		
+//		for (int i = 3; i == scan.nextInt(); i--) {
+//			String first = scan.next();
+//			String last = scan.next();
+//			int amount = scan.nextInt();
+//			int price = scan.nextInt();
+//			int totalPrice = amount * price;
+//			
+//			System.out.println(first + " " + last + ": " + totalPrice);
+			
+			
+		
+		
+		
+		
+		
+
