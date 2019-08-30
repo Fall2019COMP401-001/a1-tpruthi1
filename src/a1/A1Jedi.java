076cd111dd75ff88a16f0dd851d92057a131b513
tpruthi1 <tpruthi1@tpruthi1-pc.wireless-1x.unc.edu>
2019-08-30 09:18:31 -0400

    finished jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -9,6 +10,95 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		
+		int count = scan.nextInt();
+		
+		String[] items = new String[count*2];
+		String[] itemss = new String[count];
+		
+
+	
+		
+		int j = 0;
+		
+		for (int i=0;i<count*2;i++) {
+			items[j] = scan.next();
+			j++;
+		}
+		
+		
+		
+		for (int z=0; z<count;z++) {
+			itemss[z] = items[z*2];
+		}
+	
+		
+		
+		int people = scan.nextInt();
+		String[] firstn = new String[people];
+		String[] lastn = new String[people];
+		int[] counter = new int[count];
+		
+//		for (int k = 0; k<people; k++) {
+//			if (scan.hasNext()) {
+//				firstn[k] = scan.next();
+//				System.out.println(firstn[k]);
+//				lastn[k] = scan.next();
+//				System.out.println(lastn[k]);
+//				int numItems = scan.nextInt();
+//				System.out.println(numItems);
+//				for (int m=0; m<numItems; m++) {
+//					for (int n=0; n < count; n++) {
+//						int countOfItems = scan.nextInt();
+//						System.out.println(countOfItems);
+//						if (scan.next() == items[n]) {
+//							counter[n] = countOfItems;
+//							System.out.println(Arrays.toString(counter));
+//						}
+//					}
+//				}
+//				
+//			}
+//		}
+		
+		int[] whoGot = new int[count];
+		
+		for (int k = 0; k<people; k++) {
+			if (scan.hasNext()) {
+				firstn[k] = scan.next();
+				lastn[k] = scan.next();
+				int numItems = scan.nextInt();
+				for (int m=0; m < numItems; m++) {
+					int countOfItems = scan.nextInt();
+					String temp = scan.next();
+					for (int n = 0; n < count; n++) {
+						if (itemss[n].equals(temp)) {
+							counter[n] = counter[n] + countOfItems;
+							whoGot[n]++;
+						}
+					}
+				}
+				
+			}
+		}
+		
+		
+		
+		for (int y=0; y<count;y++) {
+			if (counter[y] == 0) {
+				System.out.println("No customers bought "+ itemss[y]);
+			} else {
+				System.out.println(whoGot[y] + " customers bought " + counter[y]+ " " + itemss[y] );
+			}
+		}
+		
+		
+			
+		
+		
+		
+		
+		
 		
 	}
 }
